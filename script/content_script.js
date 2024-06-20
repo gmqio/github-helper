707a0e2a6843ad58f8d9eab2f1f0de419ed45cce
tigerweili <tigerweili@tencent.com>
2024-06-21 00:32:14 +0800

    update

--- script/content_script.js
+++ script/content_script.js
@@ -6,6 +6,7 @@ function getSelectedTextLine(textarea) {
     return lineNumber;
 }
 
+// 获取当前登录的用户名
 function getUserName() {
     var metaTag = document.querySelector('meta[name="user-login"]');
     if (metaTag) {
@@ -15,6 +16,16 @@ function getUserName() {
     }
 }
 
+// 设置当前行的注释内容
+function setNoteContent(ele, notes_arr) {
+    for (var note of notes_arr) {
+        if ($(ele).attr('data-line-number') == note.selected_line) {
+            $('.tip').html(note.note);
+        }
+    }
+}
+
+// 获取当前页面的源代码文件信息
 function getFileInfo() {
     var filePath = location.href.split('/');
     var repo = filePath[3];
@@ -37,6 +48,7 @@ function getFileInfo() {
     
 }
 $(document).ready(function () {
+    // 右键菜单点击响应， 将选中的文本发送到backgroud script中
     document.addEventListener('contextmenu', function (e) {
         var textarea = document.getElementById('read-only-cursor-text-area');
         if (textarea && textarea === document.activeElement && textarea.selectionStart !== textarea.selectionEnd) {
@@ -58,6 +70,7 @@ $(document).ready(function () {
         }
     });
     
+    // 加载当前页面的代码注释
     chrome.runtime.sendMessage(
         {
             action: "getNotesByLine",
@@ -66,28 +79,22 @@ $(document).ready(function () {
                 ...getFileInfo()
             }
         },
+        // 从api获取到数据后，填充到页面上
         function (response) {
             var notes_arr = response.data.data;
             $('.react-blob-header-edit-and-raw-actions').eq(0).prepend('<div id="tiger_comment">已加载Tiger的评论</div>');
-            $('.react-blob-header-edit-and-raw-actions').eq(0).prepend('<div class="tip" style="display:none">hello, I am some demo comments</div>');
+            $('.react-blob-header-edit-and-raw-actions').eq(0).prepend('<div class="tip" style="display:none">loding...</div>');
 
             $('.react-line-number').each((index, lineDiv) => { // <div data-line-number="2" class="react-line-number react-code-text" style="padding-right: 16px;">2</div>
                 $(lineDiv).attr('id', 'id-line-' + $(lineDiv).text());
-                for (var note of notes_arr) {
-                    var line = note.selected_line;
-                    var content = note.note;
-
+                for (var item of notes_arr) {
+                    var line = item.selected_line;
+                    
                     if (line == $(lineDiv).text()) {
                         $(lineDiv).text(line + "-c")
-
                         $(lineDiv).hover(
                             function () {
-                                for (var note of notes_arr) {
-                                    if ($(this).attr('data-line-number') == line) {
-                                        $('.tip').html(content);
-                                    }
-                                }
-
+                                setNoteContent(this, notes_arr);
                                 $('.tip').fadeIn('slow');
                             },
                             function () {
